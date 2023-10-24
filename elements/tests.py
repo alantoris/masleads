0baa51b734d5226a3c0de965806683c7bc252550
alantoris <alan.toris@gmail.com>
2023-10-24 15:53:25 -0300

    creation of simples test responding to TDD metodology

--- elements/tests.py
+++ elements/tests.py
@@ -1,3 +1,35 @@
+from typing import Any
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class ElementsViewSetTest(TestCase):
+    """
+    Class to test list filtered and creation of Elements
+    """
+    def setUp(self):
+        self.client = APIClient()
+        self.status = 60
+        self.name = "Element Test"
+        self.wrong_status = "text"
+    
+
+    def test_list_ok(self):
+        response = self.client.get(
+            f'/elements/?status={self.status}', format="json"
+        )
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+    
+    def test_filter_not_ok(self):
+        response = self.client.get(
+            f'/elements/?status={self.wrong_status}', format="json"
+        )
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEquals(["status"], list(response.data.keys()))
+    
+    def test_create_ok(self):
+        response = self.client.post(
+            f'/elements/', dict(status=self.status, name=self.name),format="json"
+        )
+        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
